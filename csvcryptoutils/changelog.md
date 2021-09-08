5f20e54e3f0cbb2d0b2fcac1b859b4267d52d86d
dicastro <diegocastroviadero19@gmail.com>
2021-09-08 07:27:17 +0200

    - Added utility function to calculate checksum of a file using SHA3-512 algorith

---
+++ csvcryptoutils/changelog.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 1.1.0
+
+- Added utility function to calculate checksum of a file using SHA3-512 algorithm
--- csvcryptoutils/src/main/java/com/diegocastroviadero/financemanager/cryptoutils/HashUtils.java
+++ csvcryptoutils/src/main/java/com/diegocastroviadero/financemanager/cryptoutils/HashUtils.java
@@ -2,14 +2,43 @@ package com.diegocastroviadero.financemanager.cryptoutils;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class HashUtils {
     public static String getHash(final List<String[]> obj) throws IOException {
         return DigestUtils.sha3_512Hex(toByteArray(obj));
+    }
+
+    public static String getFileChecksum(final File file) throws IOException {
+        MessageDigest md;
+
+        try {
+            md = MessageDigest.getInstance("SHA3-512");
+        } catch (NoSuchAlgorithmException e) {
+            // this exception should never be thrown because algorithm is correct
+            throw new RuntimeException(e);
+        }
+
+        try (final InputStream is = new FileInputStream(file); final DigestInputStream dis = new DigestInputStream(is, md)) {
+            while (dis.read() != -1); //empty loop to clear the data
+            md = dis.getMessageDigest();
+        }
+
+        return bytesToHex(md.digest());
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        final StringBuilder sb = new StringBuilder();
+
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+
+        return sb.toString();
     }
 
     private static byte[] toByteArray(final List<String[]> data) throws IOException {
